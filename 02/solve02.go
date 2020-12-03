095f04eb4b73dabc436a0229b48974b6c9068df2
Stephen DeSanto <sfd@extrahop.com>
2020-12-02 17:58:38 -0800

    Day 02

---
+++ 02/solve02.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("input02.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	lines := []string{}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("A: %v\n", SolveA(lines))
+	fmt.Printf("B: %v\n", SolveB(lines))
+}
+
+// SolveA solves the first half
+func SolveA(input []string) int {
+	counter := 0
+	for _, line := range input {
+		policy := ParseLine(line)
+		if c := len(regexp.MustCompile(policy.letter).
+			FindAllString(policy.password, -1)); c >= policy.min && c <= policy.max {
+			counter++
+		}
+
+	}
+	return counter
+}
+
+// SolveB solves the second half
+func SolveB(input []string) int {
+	counter := 0
+	for _, line := range input {
+		policy := ParseLine(line)
+		if (string(policy.password[policy.min-1]) == policy.letter) != (string(policy.password[policy.max-1]) == policy.letter) {
+			counter++
+		}
+	}
+	return counter
+}
+
+// ParseLine parses lines
+func ParseLine(input string) PasswordPolicy {
+	re := regexp.MustCompile(`[-:\s]+`)
+	match := re.Split(input, -1)
+	return PasswordPolicy{
+		min:      MustAtoi(match[0]),
+		max:      MustAtoi(match[1]),
+		letter:   match[2],
+		password: match[3],
+	}
+}
+
+// MustAtoi is a helper
+func MustAtoi(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+// PasswordPolicy manages puzzle data
+type PasswordPolicy struct {
+	min      int
+	max      int
+	letter   string
+	password string
+}
